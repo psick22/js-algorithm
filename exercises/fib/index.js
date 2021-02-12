36f57752463830d7005f47f19826342ba0d917e5
psick22 <kgirok@gmail.com>
2021-02-13 01:54:46 +0900

    recursion & memoization 에 대해

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,40 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//   const fibArr = [];
+//   for (let i = 0; i <= n; i++) {
+//     if (i === 0 || i === 1) {
+//       fibArr.push(i);
+//     } else {
+//       let a = fibArr[i - 2] + fibArr[i - 1];
+//       fibArr.push(a);
+//     }
+//   }
+//   return fibArr[n];
+// }
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function fib(n) {
+  if (n < 2) {
+    return n;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  }
+}
+fib = memoize(fib);
+
+console.log(fib(50));
 
 module.exports = fib;
